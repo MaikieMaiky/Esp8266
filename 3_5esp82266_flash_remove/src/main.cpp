db10878b903e442c1464cbece43113b2941f7a2a
Maikie <1070606690@qq.com>
2024-08-03 20:42:27 +0800

    feat: Remove file from LittleFS flash memory
    This commit removes the specified file from the LittleFS flash memory. It initializes the flash memory system, checks if the file exists, and if so, removes it from the flash memory. The removal status is then printed to the serial monitor.

--- 3_5esp82266_flash_remove/src/main.cpp
+++ 3_5esp82266_flash_remove/src/main.cpp
@@ -1,12 +1,38 @@
-#include "main.h"
 #include <Arduino.h>
+#include <LittleFS.h>
+#include "main.h"
 
+String filename = "/B-maker/note2.txt"; // 被读取的文件位置和名称
 
 void setup()
 {
+    Serial.begin(9600);
+    Serial.println("");
+
+    if (LittleFS.begin())
+    { // 启动闪存文件系统
+        Serial.println("LittleFS Started.");
+    }
+    else
+    {
+        Serial.println("LittleFS Failed to Start.");
+    }
+
+    // 从闪存中删除file_name文件
+    if (LittleFS.remove(filename))
+    {
+
+        Serial.print(filename);
+        Serial.println(" remove sucess");
+    }
+    else
+    {
+        Serial.print(filename);
+        Serial.println(" remove fail");
+    }
 }
 
 void loop()
 {
-    // put your main code here, to run repeatedly:
+
 }
