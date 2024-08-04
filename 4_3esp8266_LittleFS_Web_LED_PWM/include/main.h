826ebb8cfe896ea433c4bb59743e554443e43b9b
Maikie <1070606690@qq.com>
2024-08-04 17:01:38 +0800

    ✨ feat: 网页上输入pwm值来调节板载LED的亮度

---
+++ 4_3esp8266_LittleFS_Web_LED_PWM/include/main.h
@@ -0,0 +1,12 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+void handleLEDControl();
+void handleUserRequest();
+bool handleFileRead(String resource);
+String getContentType(String filename);
+
+
+
+
+#endif // __MAIN_H__
